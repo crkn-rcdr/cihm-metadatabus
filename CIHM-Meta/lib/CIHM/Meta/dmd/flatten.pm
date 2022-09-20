2d45099f54fbfe8c2a12998c2a61c526e7bd47d8
Brittny Lapierre <brittny.lapierre@gmail.com>
2022-09-20 15:22:56 -0400

    Update flatten.pm

--- CIHM-Meta/lib/CIHM/Meta/dmd/flatten.pm
+++ CIHM-Meta/lib/CIHM/Meta/dmd/flatten.pm
@@ -186,19 +186,30 @@ sub marc {
 
     # We are going to look for 264 if nothing there then look in 260 field. 264 preferred source
     if ( defined $record->field('264') ) {
-        addArray( \%flat, 'pu', $record->field('264')->as_string() );
         if( defined $record->subfield( '264', 'c' ) ) {
           $flat{'pubmin'} = iso8601( $record->subfield( '264', 'c' ), 0 );
           $flat{'pubmax'} = iso8601( $record->subfield( '264', 'c' ), 1 );
         }
     } elsif ( defined $record->field('260') ) {
-        addArray( \%flat, 'pu', $record->field('260')->as_string() );
-
         if( defined $record->subfield( '260', 'c' ) ) {
           $flat{'pubmin'} = iso8601( $record->subfield( '260', 'c' ), 0 );
           $flat{'pubmax'} = iso8601( $record->subfield( '260', 'c' ), 1 );
         }
     }
+    
+    my @publisharray;
+    push @publisharray, $record->field('264');
+    push @publisharray, $record->field('260');
+
+    foreach my $publishfield ( @publisharray ) {
+      addArray( \%flat, 'pu', $publishfield->as_string() );
+      if (defined $publishfield->subfield("c") && ! defined $flat{'pubmin'}) {
+        $flat{'pubmin'} = iso8601( $publishfield->subfield("c"), 0 );
+        $flat{'pubmax'} = iso8601( $publishfield->subfield("c")  , 1 );
+      }
+    }
+
+
 
     foreach my $field ( $record->field('008') ) {
         my @lang = normalise_lang( substr( $field->as_string, 35, 3 ) );
